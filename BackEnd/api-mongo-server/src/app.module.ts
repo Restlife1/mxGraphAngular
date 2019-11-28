d8c8e213e138683773a3f512a5cc578b11ce59c4
Alumnado Tarde <restlife13@gmail.com>
2019-11-28 19:31:38 +0000

    backend v2

--- BackEnd/api-mongo-server/src/app.module.ts
+++ BackEnd/api-mongo-server/src/app.module.ts
@@ -5,7 +5,14 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule, MongooseModule.forRoot()],
+  imports: [
+    UserModule,
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: 'mongodb://localhost:27017/apiMxGraph.api'
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
